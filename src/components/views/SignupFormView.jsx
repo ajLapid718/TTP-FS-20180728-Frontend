0e0671135c54a5ae08afbf83ccaafbeceee00d8b
Allan James Lapid <ajLapid718@gmail.com>
2018-10-27 23:25:10 -0400

    feat (SignupForm): Render signup form and handle input logic

---
+++ src/components/views/SignupFormView.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const SignupFormView = props => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <label>First Name:</label>
+      <input
+        type="text"
+        name="firstName"
+        value={props.signupFields.firstName}
+        onChange={props.handleChange}
+      />
+      <br />
+      <label>Last Name:</label>
+      <input
+        type="text"
+        name="lastName"
+        value={props.signupFields.lastName}
+        onChange={props.handleChange}
+      />
+      <br />
+      <label>Email:</label>
+      <input
+        type="email"
+        name="email"
+        value={props.signupFields.email}
+        onChange={props.handleChange}
+      />
+      <br />
+      <label>Password:</label>
+      <input
+        type="password"
+        name="password"
+        value={props.signupFields.password}
+        onChange={props.handleChange}
+      />
+      <br />
+    </form>
+  );
+};
+
+export default SignupFormView;
