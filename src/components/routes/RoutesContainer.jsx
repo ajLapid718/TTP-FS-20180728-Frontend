cd3c79a5f575e055b992449f7035e984a283a17d
Allan James Lapid <ajLapid718@users.noreply.github.com>
2018-10-28 23:51:44 -0400

    Maintain user on state #11 (#12)
    * feat (currentUser): Fill out action creator, thunk, and reducer for persistent user
    
    * feat (Routes): Persist logged in user when components mount

--- src/components/routes/RoutesContainer.jsx
+++ src/components/routes/RoutesContainer.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import RoutesView from './RoutesView';
+import { me } from '../../thunks';
 
 class RoutesContainer extends Component {
+  componentDidMount() {
+    this.props.loadUser();
+  }
+
   render() {
     return <RoutesView />
   }
 }
 
-export default RoutesContainer;
+// Map dispatch to props;
+const mapDispatch = dispatch => {
+  return {
+    loadUser: () => dispatch(me())
+  }
+}
+
+export default connect(null, mapDispatch)(RoutesContainer);
