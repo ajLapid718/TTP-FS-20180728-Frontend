5f89d8d50e98dfca098487f11f96efe834092421
Allan James Lapid <ajLapid718@users.noreply.github.com>
2018-10-29 16:27:15 -0400

    feat (Portfolio): Change colors to green, red, or grey (#24)

--- src/components/views/PortfolioView.jsx
+++ src/components/views/PortfolioView.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import './styles/PortfolioView.css';
 
 const PortfolioView = (props) => {
   return (
     <div>
       <h1>Personal Portfolio</h1>
       {props.personalPortfolio.stocks
-      ? props.personalPortfolio.stocks.map(stock => (
-          <div>
-            <h4>{stock.tickerSymbol} - {stock.amountOfShares} Shares | {stock.pricePerShare} | Current Price: {stock.currentPrice} </h4>
-          </div>))
+      ? props.personalPortfolio.stocks.map(stock => {
+          let performanceIndicator;
+          let difference = stock.currentPrice - stock.pricePerShare;
+          console.log(stock.currentPrice, stock.pricePerShare)
+          if (difference < 0) performanceIndicator = "negative";
+          if (difference === 0) performanceIndicator = "neutral";
+          if (difference > 0) performanceIndicator = "positive";
+
+          return <div>
+            <h4> <span className={performanceIndicator}>{stock.tickerSymbol}</span> - {stock.amountOfShares} Shares | {stock.pricePerShare} | Current Value: <span className={performanceIndicator}>{stock.currentPrice * stock.amountOfShares}</span> </h4>
+          </div>})
       : null}
     </div>
   );
