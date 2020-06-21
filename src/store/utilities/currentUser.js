f63d9d91918c93a01171128b50c800e743461411
Allan James Lapid <ajLapid718@gmail.com>
2020-06-21 19:50:50 -0400

    typoe

--- src/store/utilities/currentUser.js
+++ src/store/utilities/currentUser.js
@@ -49,7 +49,7 @@ export const loginUserThunk = (email, password) => dispatch => {
 
 export const me = () => async dispatch => {
   try {
-    const res = await axios.get(`${BASE_URL}/auth/me`), { withCredentials: true };
+    const res = await axios.get(`${BASE_URL}/auth/me`, { withCredentials: true });
     dispatch(getUser(res.data || {}));
   }
   catch (err) {
