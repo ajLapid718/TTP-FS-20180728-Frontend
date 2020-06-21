056d95d4d6e5f14a6447fbf67aaaf40fa9162ec2
Allan James Lapid <ajLapid718@gmail.com>
2020-06-21 19:06:06 -0400

    withCred nah

--- src/store/utilities/currentUser.js
+++ src/store/utilities/currentUser.js
@@ -48,7 +48,7 @@ export const loginUserThunk = (email, password) => dispatch => {
 
 export const me = () => async dispatch => {
   try {
-    const res = await axios.get(`${BASE_URL}/auth/me`, { withCredentials: true });
+    const res = await axios.get(`${BASE_URL}/auth/me`);
     dispatch(getUser(res.data || {}));
   }
   catch (err) {
