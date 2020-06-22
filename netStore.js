052e356b68cb8a35dc3907bef22e414c8ce39771
Allan James Lapid <ajLapid718@gmail.com>
2020-06-22 00:59:37 -0400

    star

---
+++ netStore.js
@@ -0,0 +1,11 @@
+// [[redirects]]
+//   from = "/api/*"
+//   to = "https://workforce-stock-server.herokuapp.com/:splat"
+//   status = 200
+//   headers = {X-FROM = "https://serene-varahamihira-fbc247.netlify.app"}
+
+// [[redirects]]
+//   from = "/auth/*"
+//   to = "https://workforce-stock-server.herokuapp.com/:splat"
+//   status = 200
+//   headers = {X-FROM = "https://serene-varahamihira-fbc247.netlify.app"}
